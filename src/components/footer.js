69d0e6da58070c61b3d943c89155290beee43d2e
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-01-06 16:31:23 -0500

    manasseh's changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,67 @@
+import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import tw, { styled } from "twin.macro"
+import PropTypes from "prop-types"
 import React from "react"
+import Img from "gatsby-image"
 
-const Footer = () => (
-  <footer
-    style={{
-      marginTop: `2rem`,
-    }}
-  >
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.com">Gatsby</a>
-  </footer>
-)
+const StyledNav = tw.nav`
+  flex justify-between
+`
+const StyledLink = styled(props => <Link {...props} />)`
+  ${tw`mr-8 hover:text-purple-900 hover:border-b-2`}
+`
+
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "resinde-splash-transparent.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          fixed(width: 256, height: 125) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <footer>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <StyledNav>
+          <div>
+            <Link to="/">
+              <Img fixed={data.file.childImageSharp.fixed} alt=" " />
+            </Link>
+          </div>
+          <div>
+            <p> Made with </p> <img src="favorite.png" alt="love"></img>{" "}
+            <p> by ResInDe </p>
+          </div>
+          <div>
+            <img src="facebook.png" alt="facebook"></img>
+            <img src="linkedin.png" alt="linkedin"></img>
+            <p>© {new Date().getFullYear()} ResInDe. All rights reserved.</p>
+          </div>
+        </StyledNav>
+      </div>
+    </footer>
+  )
+}
+
+Footer.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Footer.defaultProps = {
+  siteTitle: ``,
+}
 
 export default Footer
