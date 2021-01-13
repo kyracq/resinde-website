198751ba1852a14bf250219a9747a46921a32060
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-01-13 16:02:42 -0500

    about us

--- src/components/about-section.js
+++ src/components/about-section.js
@@ -1,10 +1,25 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import tw from "twin.macro"
-import BackgroundImage from "gatsby-background-image"
+import tw, { styled } from "twin.macro"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
 
-const StyledH1 = tw.h1`
-  text-white text-xl items-center
+const StyledH1 = styled.h1`
+  ${tw`text-white text-7xl items-center absolute`}
+  top: 40%;
+  left: 100px;
+`
+
+const StyledDiv = styled.div`
+  ${tw`flex`}
+`
+
+const SectionText = styled.div`
+  ${tw`self-center flex-grow ml-8`}
+`
+
+const StyledButton = styled(props => <Link {...props} />)`
+  ${tw`border-blue rounded-full text-blue py-2 px-8 uppercase border`}
 `
 
 const AboutSection = props => {
@@ -12,27 +27,35 @@ const AboutSection = props => {
     query {
       placeholderImage: file(relativePath: { eq: "blue-blob.png" }) {
         childImageSharp {
-          fluid(maxWidth: 500) {
-            ...GatsbyImageSharpFluid
+          fixed(width: 500) {
+            ...GatsbyImageSharpFixed
           }
         }
       }
     }
   `)
 
-  if (!data?.placeholderImage?.childImageSharp?.fluid) {
+  if (!data?.placeholderImage?.childImageSharp?.fixed) {
     return <div>Picture not found</div>
   }
 
   return (
-    <section id={props.id}>
-      <BackgroundImage
-        fluid={data.placeholderImage.childImageSharp.fluid}
-        role=""
-      >
+    <StyledDiv>
+      <div id={props.id} style={{ position: "relative" }}>
+        <Img fixed={data.placeholderImage.childImageSharp.fixed} />
         <StyledH1>About Us</StyledH1>
-      </BackgroundImage>
-    </section>
+      </div>
+      <SectionText>
+        <p>
+          A group at Princeton University focused on Research, Innovation, and
+          Design. As a team of designers and developers, our mission is to drive
+          impactful user experience solutions through user research and digital
+          implementation.
+        </p>
+        <br />
+        <StyledButton to="/about">learn more</StyledButton>
+      </SectionText>
+    </StyledDiv>
   )
 }
 
