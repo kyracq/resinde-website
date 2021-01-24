39f602937bd21714505ae745e679cc900ffd345a
is-1 <65904475+is-1@users.noreply.github.com>
2021-01-24 13:06:47 -0500

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,38 @@
-import React from "react"
-
+import React, { useState, useEffect } from "react"
+import tw, { styled } from "twin.macro"
 import SEO from "../components/seo"
-
+ 
+const StyledInput = styled.input`
+  ${tw`rounded-xl my-7 px-6 py-2 block w-full`}
+`
+const StyledTextarea = styled.textarea`
+  ${tw`rounded-xl my-5 px-6 py-6 block w-full`}
+`
+const StyledButton = styled.button`
+  ${tw`rounded-xl p-4 bg-purple hover:bg-magenta`}
+`
 const ContactPage = () => (
   <div>
     <SEO title="Contact Us" />
-    <h1>Hi from the contact us page</h1>
+    <h1>Contact Us</h1>
+    <form action="/contact-form" method="post">
+      <div>
+        <StyledInput style={{ border: `3px solid #7d71f2`}} placeholder="Name" required></StyledInput>
+      </div>
+      <div>
+        <StyledInput style={{ border: `3px solid #f02e65`}} placeholder="Email Address" required></StyledInput>
+      </div>
+      <div>
+        <StyledTextarea style={{ border: `3px solid #0148e8`}} rows="5" placeholder="Message" required></StyledTextarea>
+      </div>
+      <div>
+        <StyledButton>SUBMIT</StyledButton>
+      </div>
+    </form>
+    <div style={{marginTop: `3rem`, marginBottom: `1.45rem`}}>Join out mailing list for monthly updates about opportunities and what's going on in ResInDe!</div>
+    <div>
+      <StyledButton>Join the Newsletter!</StyledButton>
+    </div>
   </div>
 )
-
 export default ContactPage
