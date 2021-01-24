11b192812106efe1f8c5e9982a507f2900291268
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-01-24 09:32:59 -0500

    Mobile nav, about page

---
+++ src/components/mobile-nav.js
@@ -0,0 +1,150 @@
+import tw, { styled } from "twin.macro"
+import React, { useState } from "react"
+import { Link } from "gatsby"
+
+const MenuDiv = styled.div`
+  padding-top: 3rem;
+`
+const ContainerDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: ${props => (props.open ? `100%` : `0`)};
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  opacity: 1;
+  color: #fafafa;
+  transition: height 0.3s ease;
+  z-index: 2;
+`
+
+const StyledDiv = styled.div`
+  ${tw`sm:hidden`}
+  height: 32px;
+  width: 32px;
+  display: flex;
+  padding: 4px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const TopLine = styled.div`
+  height: 2px;
+  width: 20px;
+  background-color: ${props => props.color};
+  transition: all 0.2s ease;
+  transform: ${props => (props.open ? `rotate(45deg)` : `none`)};
+  transform-origin: top left;
+  margin-bottom: 5px;
+  z-index: 9;
+`
+const MiddleLine = styled.div`
+  height: 2px;
+  width: 20px;
+  background-color: ${props => props.color};
+  transition: all 0.2s ease;
+  opacity: ${props => (props.open ? `0` : `1`)};
+  transform: ${props => (props.open ? `translateX(-16px)` : `none`)};
+  z-index: 9;
+`
+
+const BottomLine = styled.div`
+  height: 2px;
+  width: 20px;
+  background-color: ${props => props.color};
+  transition: all 0.2s ease;
+  transform: ${props =>
+    props.open ? `translateX(-1px) rotate(-45deg)` : `none`};
+  transform-origin: top left;
+  margin-top: 5px;
+  z-index: 9;
+`
+
+const MenuItemDiv = styled.div`
+  font-size: 1.2rem;
+  padding: 1rem 0;
+  margin: 0 5%;
+  animation-delay: ${props => props.delay};
+`
+
+const LineDiv = styled.div`
+  width: 90%;
+  height: 1px;
+  background-color: gray;
+  margin: 0 auto;
+  animation-delay: ${props => props.delay};
+`
+
+const MobileNav = props => {
+  const [open, setOpen] = useState(false)
+  const [color, setColor] = useState("black")
+  const toggle = () => {
+    setOpen(open => !open)
+    setColor(color => {
+      if (color === "black") return "white"
+      return "black"
+    })
+  }
+
+  const navLinks = [
+    { pageName: "About", pageUrl: "/about" },
+    { pageName: "Work", pageUrl: "/work" },
+    { pageName: "Our Team", pageUrl: "/team" },
+    { pageName: "For Students", pageUrl: "/for-students" },
+    { pageName: "Contact Us", pageUrl: "/contact" },
+  ]
+
+  const menuItems = navLinks.map((value, index) => {
+    return (
+      <MenuItem key={index} delay={`${index * 0.1}s`}>
+        <Link onClick={toggle} to={value.pageUrl}>
+          {value.pageName}
+        </Link>
+      </MenuItem>
+    )
+  })
+
+  return (
+    <div>
+      <MenuButton open={open} toggle={toggle} color={color} />
+      <Menu open={open}>{menuItems}</Menu>
+    </div>
+  )
+}
+
+const MenuItem = props => {
+  return (
+    <div>
+      <MenuItemDiv delay={props.delay}>{props.children}</MenuItemDiv>
+      <LineDiv delay={props.delay} />
+    </div>
+  )
+}
+
+const Menu = props => {
+  if (props.open) {
+    return (
+      <ContainerDiv open={props.open}>
+        <MenuDiv>{props.children}</MenuDiv>
+      </ContainerDiv>
+    )
+  } else {
+    return <ContainerDiv open={props.open} />
+  }
+}
+
+const MenuButton = props => (
+  <StyledDiv onClick={props.toggle}>
+    <TopLine color={props.color} open={props.open} />
+    <MiddleLine color={props.color} open={props.open} />
+    <BottomLine color={props.color} open={props.open} />
+  </StyledDiv>
+)
+
+MobileNav.propTypes = {}
+
+MobileNav.defaultProps = {}
+
+export default MobileNav
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,138 @@
 import React from "react"
-
 import SEO from "../components/seo"
+import tw, { styled } from "twin.macro"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const AboutPage = () => (
-  <div>
-    <SEO title="About" />
-    <h1>Hi from the about page</h1>
-    <p>Welcome to page 2</p>
-  </div>
-)
+const StyledHeaderDiv = styled.div`
+  ${tw`sm:px-32 mt-16 px-8`}
+`
+const StyledDiv = styled.div`
+  ${tw`flex`}
+`
+
+const Header1 = styled.h1`
+  ${tw`text-white text-7xl absolute`}
+  top: 300px;
+  left: 70px;
+`
+
+const Header2 = styled.h1`
+  ${tw`text-white text-7xl absolute`}
+  top: 200px;
+  left: 70px;
+`
+
+const StyledTextDiv = styled.div`
+  ${tw`mb-8 px-20`}
+`
+
+const SectionText = styled.div`
+  ${tw`self-center flex-grow mr-8`}
+  div {
+    ${tw`px-20`}
+  }
+`
+
+const StyledList = tw.ul`
+  list-disc list-inside
+`
+
+const NumberedList = tw.ul`
+  list-decimal list-inside
+`
+
+const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      pinkBlob: file(relativePath: { eq: "pink-blob-2.png" }) {
+        childImageSharp {
+          fixed(width: 500) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      yellowBlob: file(relativePath: { eq: "yellow-blob-2.png" }) {
+        childImageSharp {
+          fixed(width: 500) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <div>
+      <SEO title="About" />
+      <StyledHeaderDiv>
+        <h1>About Us</h1>
+        <p>
+          A group at Princeton University focused on Research, Innovation, and
+          Design. As a team of designers and developers, our mission is to drive
+          impactful user experience solutions through user research and digital
+          implementation.
+        </p>
+      </StyledHeaderDiv>
+      <StyledDiv>
+        <div style={{ position: "relative" }}>
+          <Img fixed={data.pinkBlob.childImageSharp.fixed} />
+          <Header1>Services</Header1>
+        </div>
+        <SectionText>
+          <StyledTextDiv>
+            <StyledList>
+              <li>User Research</li>
+              <li>Product Strategy</li>
+              <li>UI/UX Design</li>
+            </StyledList>
+          </StyledTextDiv>
+        </SectionText>
+      </StyledDiv>
+      <StyledDiv>
+        <SectionText>
+          <StyledTextDiv>
+            Princeton ResInDe focuses on building projects with real impact on
+            our community. As a team member, you’ll be working with a team to
+            complete a project ranging from conducting user research, building
+            prototypes, developing product strategies, and software development.
+            In addition to projects, you’ll take part in teamwide events such
+            as:
+            <StyledList>
+              <li>
+                Project syncs where each team shares their work with other teams
+              </li>
+              <li>
+                Brainstorming events where the team brainstorms potential
+                problems on and off campus to address
+              </li>
+              <li>Fun, social events!</li>
+            </StyledList>
+            <br />
+            Princeton ResInDe is founded to bring value to you in several ways:
+            <NumberedList>
+              <li>
+                To immerse yourself in the world of practical and design
+                thinking.
+              </li>
+              <li>
+                To work on meaningful projects that generate a lasting impact on
+                local communities.
+              </li>
+              <li>
+                To amp up your resume and portfolio. To join a community of
+                designers, entrepreneurs, and innovative thinkers.
+              </li>
+            </NumberedList>
+          </StyledTextDiv>
+        </SectionText>
+        <div style={{ position: "relative" }}>
+          <Img fixed={data.yellowBlob.childImageSharp.fixed} />
+          <Header2>Supporters</Header2>
+        </div>
+      </StyledDiv>
+    </div>
+  )
+}
 
 export default AboutPage
