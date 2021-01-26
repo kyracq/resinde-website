1d7e0d1a11a8577b79b16515e9b5ff3cea4474c2
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-01-25 19:34:11 -0500

    ok

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,7 @@ const HeroContainer = tw.div`
   flex flex-wrap justify-center items-center px-32 mt-32
 `
 
-const StyledDiv = tw.div`
+const StyledA = tw.a`
   flex space-x-4 text-left
 `
 
@@ -21,9 +21,11 @@ const HeroSection = props => {
     <div>
       <HeroContainer>
         <HeroContent>
-          <h1>Site under construction.</h1>
-          <p>Check back soon.</p>
-          <StyledDiv>
+          <div>
+            <h1>Site under construction.</h1>
+            <p>Check back soon.</p>
+          </div>
+          <StyledA href="mailto:princetonresinde@gmail.com">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -39,10 +41,26 @@ const HeroSection = props => {
               <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
               <polyline points="22,6 12,13 2,6"></polyline>
             </svg>
-            <a href="mailto:princetonresinde@gmail.com">
-              princetonresinde@gmail.com
-            </a>
-          </StyledDiv>
+            <div>princetonresinde@gmail.com</div>
+          </StyledA>
+          <StyledA href="https://docs.google.com/forms/d/e/1FAIpQLSefvNUo-nYvVbaGatCi5z8URA9WmKqLhjfhBizIxmAj8YtAFQ/viewform">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-edit-3"
+            >
+              <path d="M12 20h9"></path>
+              <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+            </svg>
+            <div>Apply to join the ResInDe team</div>
+          </StyledA>
         </HeroContent>
       </HeroContainer>
     </div>
