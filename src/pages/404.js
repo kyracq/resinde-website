7c5fa50fffa77c1cccc005f43ab55ca8159fa9a0
Kyra Acquah <20865277+kyracq@users.noreply.github.com>
2021-02-05 13:45:49 -0500

    form.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,11 @@
 import React from "react"
 import tw, { styled } from "twin.macro"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+
+const StyledLink = styled(props => <Link {...props} />)`
+  ${tw`text-blue`}
+`
 
 const HeroContainer = tw.div`
   flex flex-col min-h-screen max-w-max justify-center mobile:px-8 px-48
@@ -11,6 +16,7 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <StyledLink to="/">Back to the homepage.</StyledLink>
   </HeroContainer>
 )
 
--- src/pages/contact/thanks.js
+++ src/pages/contact/thanks.js
@@ -1,5 +1,10 @@
 import React from "react"
-import tw from "twin.macro"
+import tw, { styled } from "twin.macro"
+import { Link } from "gatsby"
+
+const StyledLink = styled(props => <Link {...props} />)`
+  ${tw`text-blue`}
+`
 
 const HeroContainer = tw.div`
   flex flex-col min-h-screen max-w-max justify-center mobile:px-8 px-48
@@ -9,6 +14,7 @@ const NotFoundPage = () => (
   <HeroContainer>
     <h1>Thank you!</h1>
     <p>We'll get back to you soon.</p>
+    <StyledLink to="/">Back to the homepage.</StyledLink>
   </HeroContainer>
 )
 
